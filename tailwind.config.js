050837d2f483ea06d5040ee5aeba5ea35744d4fd
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-04-21 18:54:13 +0300

    added the images folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      "white": "#ffffff",
+      "black": "#000000",
+      "coffee": "#6F4E37",
+      "reddish": "#ff4000",
+    },
     extend: {},
   },
   plugins: [],
