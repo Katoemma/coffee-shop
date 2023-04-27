7e3cc13f513ec2a91f63d2c97eb8765ff7ae4680
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-04-27 19:30:49 +0300

    added the script to run the purchase cards

--- public/js/index.js
+++ public/js/index.js
@@ -21,3 +21,19 @@ function closeMenu(){
   document.getElementById("navLinks").style.display = "none";
   document.getElementById("navLinks").style.backgroundColor = "none";
 }
+
+
+//for the order page to trigger the order card
+function openPurchase(){
+  document.getElementById("orderpurc").style.display = "flex";
+}
+function cancelOrder(){
+  document.getElementById("orderpurc").style.display = "none";
+}
+//calculte the amount of cups on the order
+function totalPrice(){
+  let price = document.getElementById("price");
+  let cups = document.getElementById("totalCups").value;
+
+  price.innerHTML = 1500 * cups;
+}
