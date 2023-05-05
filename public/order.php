7edc881b697f9fd2ed3b3885b5feb4012cb32c76
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-05-05 12:55:05 +0300

    re-installed tailwindcss

--- public/order.php
+++ public/order.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Coffee</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!--font awesome link-->
-    <link rel="stylesheet" href="../css/styles.css">
+    
     <!--AOS links-->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
